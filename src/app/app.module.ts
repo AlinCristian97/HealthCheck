173f73e55acccbf985ec0b384da505f93a90b431
Alin Apostol <alin.cristian977@gmail.com>
2022-06-22 00:34:55 +0300

    Revert "added health check component and ts implementation"
    This reverts commit 3692a662cb94993654fb2947d63811792df17d1f.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HealthCheckComponent } from './health-check/health-check.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FetchDataComponent,
-    NavMenuComponent,
-    HealthCheckComponent
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
